87b2a361c25e25f7fed6d8ad715867fa07b0096f
vaishali <vaishali.ds0228@gmail.com>
2025-07-01 23:50:36 +0530

    asyncHandler,routes,and controller

--- src/app.js
+++ src/app.js
@@ -11,5 +11,21 @@ app.use(cors({
 app.use(express.json({limit: "20kb"}))
 app.use(express.urlencoded({extended:true}))
 app.use(express.static("public"))
+app.use(cookieParser())
+
+
+//routes
+import userRouter from "./routes/user.routes.js"
+
+
+//cors 
+// app.use((req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', '*');
+//   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+//   res.header('Access-Control-Allow-Headers', 'Content-Type');
+//   next();
+// });
+//declaration
+app.use("/api/v1/users", userRouter)
 
 export  { app }
